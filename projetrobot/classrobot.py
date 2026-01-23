f0b4b0153afd92f7ad9bb80fa3cb43195f5d1cfa
x1angkun05 <xiangkunssr15@gmail.com>
2026-01-23 20:26:23 +0800

    Update classrobot.py
    un test pour la fonction

--- projetrobot/classrobot.py
+++ projetrobot/classrobot.py
@@ -10,3 +10,10 @@ class Robot:
         radian_angle = math.radians(self.angle)
         self.x += vitesse * duree * math.cos(radian_angle)
         self.y += vitesse * duree * math.sin(radian_angle)
+    def get_position(self):
+        return (self.x, self.y, self.angle)
+print ("-------------test class Robot-----------------")
+robot1 = Robot("best_robot")
+robot1.move(10, 2, 90)
+position = robot1.get_position()
+print(f"Robot Position: x={position[0]}, y={position[1]}, angle={position[2]}")
