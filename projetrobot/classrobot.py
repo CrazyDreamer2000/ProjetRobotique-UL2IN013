326f3390f16fba3683b8bf4141592973d9a86604
x1angkun05 <xiangkunssr15@gmail.com>
2026-01-23 20:19:58 +0800

    Update classrobot.py
    definir la fonction move

--- projetrobot/classrobot.py
+++ projetrobot/classrobot.py
@@ -1,6 +1,12 @@
+import math
 class Robot:
     def __init__(self, name, x=0, y=0, angle=0):
         self.name = name
         self.x = x
         self.y = y
         self.angle = angle
+    def move(self, vitesse, duree, angle):
+        self.angle = angle
+        radian_angle = math.radians(self.angle)
+        self.x += vitesse * duree * math.cos(radian_angle)
+        self.y += vitesse * duree * math.sin(radian_angle)
