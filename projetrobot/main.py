c96c487414d9d2bafcf451219b639c0a3eca33e8
x1angkun05 <xiangkunssr15@gmail.com>
2026-01-26 04:34:00 +0800

    run le code

--- projetrobot/main.py
+++ projetrobot/main.py
@@ -6,16 +6,16 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600)) # taille de la fenêtre
 clock = pygame.time.Clock() # pour gérer le temps
 
-robot = Robot('best_robot', 400， 300)
+robot = Robot('best_robot', 400, 300)
 running = True
 
-while running
+while running:
     "Acquisition d'intervalle de temps"
-    dt = clock.tick(60) / 100.0
+    dt = clock.tick(60) / 500.0
 
     for event in pygame.event.get() :
         if event.type == pygame.QUIT:
-            running == False
+            running = False
     
     keys = pygame.key.get_pressed()
     v_lin = 0
@@ -26,9 +26,9 @@ while running
     if keys[pygame.K_DOWN]:
         v_lin = -100
     if keys[pygame.K_LEFT]:
-        v_ang = 5
-    if keys[pygame.K_RIGHT]:
         v_ang = -5
+    if keys[pygame.K_RIGHT]:
+        v_ang = 5
     
     robot.update_velocity(v_lin, v_ang)  
 
