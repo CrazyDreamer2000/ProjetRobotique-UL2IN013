4f9a09f440dae585dd288250bfdcd4c18f580111
x1angkun05 <xiangkunssr15@gmail.com>
2026-01-26 03:15:20 +0800

    Modifier les données de base

--- projetrobot/classrobot.py
+++ projetrobot/classrobot.py
@@ -1,20 +1,32 @@
 import math
 class Robot:
     def __init__(self, name, x=0, y=0, angle=0):
+        # Initialisation des attributs du robot
         self.name = name
         self.x = x
         self.y = y
-        self.angle = angle
-    def move(self, vitesse, duree, angle):
-        self.angle = angle
-        radian_angle = math.radians(self.angle)
+        self.angle = math.radians(angle)
 
-        new_x = vitesse * duree * math.cos(radian_angle)
-        new_y = vitesse * duree * math.sin(radian_angle)
+        self.v_linear = 0   # set une vitesse linéaire initiale à 0
+        self.v_angular_velocity = 0  # set un vitesse angulaire initiale à 0
+    
+    
+    def move(self, duree):
+        self.angle = self.angle
+
+        new_x = vitesse * duree * self.angle
+        new_y = vitesse * duree * self.angle
+
+        self.y += new_y
+        self.x += new_x
         self.x = round(new_x,5)
         self.y = round(new_y,5)
+    
+    
     def get_position(self):
         return (self.x, self.y, self.angle)
+
+
 print ("-------------test class Robot-----------------")
 robot1 = Robot("best_robot")
 robot1.move(10, 2, 90)
