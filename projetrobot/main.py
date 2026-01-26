dda0d97095e564afa1c414498d14325680e9c878
x1angkun05 <xiangkunssr15@gmail.com>
2026-01-26 13:01:29 +0800

    Update main.py
    ajouter tancer une carre

--- projetrobot/main.py
+++ projetrobot/main.py
@@ -1,5 +1,6 @@
 from classrobot import Robot
 import pygame
+import math
 
 # Initialisation de Pygame
 pygame.init()
@@ -8,10 +9,11 @@ clock = pygame.time.Clock() # pour gérer le temps
 
 robot = Robot('best_robot', 400, 300)
 running = True
+frame_counter = -1
 
 while running:
     "Acquisition d'intervalle de temps"
-    dt = clock.tick(60) / 500.0
+    dt = clock.tick(60) / 1000.0
 
     for event in pygame.event.get() :
         if event.type == pygame.QUIT:
@@ -29,6 +31,22 @@ while running:
         v_ang = -5
     if keys[pygame.K_RIGHT]:
         v_ang = 5
+
+    keys2 = pygame.key.get_just_pressed()
+
+    if keys2[pygame.K_1]:
+        frame_counter = 0
+    
+    if  frame_counter >= 0:
+        frame_counter += 1
+        stage = frame_counter // 40
+
+        if stage in [0, 2, 4, 6]:
+            v_lin = 200
+        if stage in [1, 3 ,5]:
+            v_ang = (math.pi / 2) / (40 * dt)
+        if frame_counter == 280:
+            frame_counter = -1
     
     robot.update_velocity(v_lin, v_ang)  
 
