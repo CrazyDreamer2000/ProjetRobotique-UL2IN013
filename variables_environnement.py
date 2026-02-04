99b02513bf8a6b3bdd3abc094b29586be932addf
Mateusz Wieckowski <48412994+CrazyDreamer2000@users.noreply.github.com>
2026-02-04 15:44:54 +0100

    Add environment variables for robot simulation
    Centralize all constants and add boost variables
    - Add boost multipliers (MULTI_VITESSE_LIN, MULTI_VITESSE_ANG)
    - Add MODE_CTRL to represent boost state
    - Add explanatory comments for each constant

---
+++ variables_environnement.py
@@ -0,0 +1,33 @@
+LARGEUR_ECRAN = 800  # Largeur de la fenêtre en pixels
+HAUTEUR_ECRAN = 600  # Hauteur de la fenêtre en pixels
+FPS = 60  # Images par seconde
+
+# Robot
+ROBOT_TAILLE_X = 100  # Largeur du rectangle robot en pixels
+ROBOT_TAILLE_Y = 100  # Hauteur du rectangle robot en pixels
+ROBOT_COULEUR = (0, 0, 255)  # Couleur RGB du robot (bleu)
+ROBOT_TETE_LONGUEUR = 30  # Longueur de la tête/direction en pixels
+ROBOT_TETE_EPAISSEUR = 5  # Épaisseur de la ligne tête en pixels
+ROBOT_TETE_COULEUR = (255, 0, 0)  # Couleur RGB de la tête (rouge)
+
+# Mouvement
+VITESSE_LIN = 150  # Vitesse linéaire au clavier (pixels/s)
+VITESSE_LIN_AUTO = 250  # Vitesse linéaire en mode automatique (pixels/s) (Tracer carre)
+VITESSE_ANG = 8  # Vitesse angulaire (radians/s)
+
+# Obstacles
+TAILLE_OBSTACLE = 60  # Taille des obstacles carrés en pixels
+COULEUR_OBSTACLE = (101, 69, 19)  # Couleur RGB des obstacles (marron)
+
+# Trajectoire
+COULEUR_TRAJECTOIRE = (0,0,0)  # Couleur RGB de la trajectoire (vert)
+EPAISSEUR_TRAJECTOIRE = 3  # Épaisseur de la ligne de trajectoire en pixels
+
+#CHANGEMENT VITESSE
+MODE_CTRL = False
+MULTI_VITESSE_LIN = 2
+MULTI_VITESSE_ANG = 2
+
+
+
+
