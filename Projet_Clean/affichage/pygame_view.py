958d4a5fdbb37c0c7140ad721d388bb663627943
alexargeles <guapelididoo@gmail.com>
2026-02-10 18:15:15 +0100

    Ajout de commentaires et spécification pour les types "irl" des variables

--- Projet_Clean/affichage/pygame_view.py
+++ Projet_Clean/affichage/pygame_view.py
@@ -3,32 +3,34 @@ import math
 
 SCALE = 200  # pixels par mètre
 
-ROBOT_L = 0.30 # Longueur
-ROBOT_l = 0.20 # largeur
-ECARTEMENT_ROUES = 0.15
+ROBOT_L = 0.30 # mètres : Longueur
+ROBOT_l = 0.20 # mètres : largeur
+ECARTEMENT_ROUES = 0.15 # mètres
 
 def transformer_point(x, y, cx, cy, angle):
     """Transforme position des points du robot en points sur le repère
-    x, y: Coordonnées d'un point dans le repère du robot (ex: (x=20, y=10) = 20 pixels devant le robot, 10 pixels à gauche)
-    cx, cy: Coordonnées du centre du robot dans le monde
-    xr, yr: x et y après rotation"""
+    x, y: Coordonnées d'un point dans le repère du robot en pixels (ex: (x=20, y=10) = 20 pixels devant le robot, 10 pixels à gauche)
+    cx, cy: Coordonnées du centre du robot dans le monde (pixels)
+    xr, yr: x et y après rotation (pixels)"""
     xr = x * math.cos(angle) - y * math.sin(angle) # Formule de rotation de points en maths
     yr = x * math.sin(angle) + y * math.cos(angle) #
     return cx + xr, cy + yr
 
 def dessiner_robot(screen, robot):
-    cx = robot.pos.x * SCALE
-    cy = robot.pos.y * SCALE
+    """Affiche le robot sur pygame"""
+    cx = robot.pos.x * SCALE # pixels (mètres -> pixels avec *SCALE)
+    cy = robot.pos.y * SCALE # pixels
     ori = robot.pos.orientation
 
-    L = ROBOT_L * SCALE / 2 # de mètres -> en pixels, puis /2 pour définir les coins
+    L = ROBOT_L * SCALE / 2 # pixels (mètres -> pixels avec *SCALE), puis /2 pour ensuite définir les coins
     l = ROBOT_l * SCALE / 2 #
 
-    coins = [(-L, -l), (L, -l), (L, l), (-L, l)]
-    points = [transformer_point(x, y, cx, cy, ori) for x, y in coins]
+    coins = [(-L, -l), (L, -l), (L, l), (-L, l)] # pixels (repère robot)
+    points = [transformer_point(x, y, cx, cy, ori) for x, y in coins]  # pixels (repère écran)
 
-    pygame.draw.polygon(screen, (80, 130, 200), points)
-
+    pygame.draw.polygon(screen, (80, 130, 200), points) # pour dessiner le robot sur pygame
+                                                        # on utilise polygon et pas rect pour afficher un rectangle pas forcemment aligné à l'écran
     # direction avant
-    fx, fy = transformer_point(L, 0, cx, cy, ori)
-    pygame.draw.line(screen, (255, 0, 0), (cx, cy), (fx, fy), 2)
+    fx, fy = transformer_point(L, 0, cx, cy, ori) # pixels (devant du robot)
+    pygame.draw.line(screen, (255, 0, 0), (cx, cy), (fx, fy), 2) # dessiner la direction de l'ecran
+
