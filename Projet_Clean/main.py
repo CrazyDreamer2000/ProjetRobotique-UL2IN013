ca205da054ad1560868fb63eb8eec8b2507681c6
Rayane YOUSFI <rayaneoneplus@gmail.com>
2026-02-10 22:08:57 +0100

    Definition et implémentation de la fonction get_corners(), permet d'obtenir les 4 coins d'un Obstacle

--- Projet_Clean/main.py
+++ Projet_Clean/main.py
@@ -3,7 +3,7 @@ import config as cfg
 from core.robot import Robot
 from monde import Obstacle
 from controle.AlgoCarre import AlgoCarre
-from affichage.pygame_view import dessiner_robot, dessiner_obstacles
+from affichage.pygame_view import dessiner_robot
 
 largeur, hauteur = 900, 600 # = 4.5 mètres * 3 mètres
 
@@ -17,7 +17,7 @@ robot.pos.y = (hauteur / 2) / cfg.SCALE #
 
 algo = AlgoCarre()
 obstacles=[]
-
+corners=[]
 running = True
 while running:
     dt = clock.tick(60) / 1000.0 # on divise par 1000 pour avoir la valeur en secondes (milisecondes -> secondes)
@@ -29,9 +29,10 @@ while running:
             obs_mousex,obs_mousey= pygame.mouse.get_pos()
             obstacle = Obstacle(obs_mousex,obs_mousey,cfg.TAILLE_OBSTACLE,cfg.TAILLE_OBSTACLE)
             obstacles.append(obstacle)
-            
-            #for obs in obstacles: Test des instances dans la classe Obstacle
-            #    print(obs.x,obs.y,obs.longueur,obs.largeur)
+            for obs in obstacles:       
+                print(obs.x,obs.y,obs.longueur,obs.largeur)
+                corners.append(obs.get_corners())
+                print(corners)
                 
     vg, vd = algo.calculer_commande(robot, dt)
     robot.definir_commande_roues(vg, vd)
--- Projet_Clean/monde.py
+++ Projet_Clean/monde.py
@@ -4,5 +4,13 @@ class Obstacle:
     def __init__(self,x,y,largeur,longueur):
         self.x= x
         self.y= y
-        self.largeur = largeur // 2
-        self.longueur = longueur // 2  
+        self.largeur = largeur
+        self.longueur = longueur
+
+    def get_corners(self):
+        #Permet d'obtenir la position des 4 coins de l'obstacle
+        bottom_left=(self.x-self.longueur/2,self.y-self.largeur/2)
+        bottom_right=(self.x+self.longueur/2,self.y-self.largeur/2)
+        upper_left=(self.x-self.longueur/2,self.y+self.largeur/2)
+        upper_right=(self.x+self.longueur/2,self.y+self.largeur/2)
+        return bottom_left,bottom_right,upper_left,upper_right
