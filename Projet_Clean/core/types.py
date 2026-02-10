824c587e876d0ba7becefca50026ff851e48e890
alexargeles <guapelididoo@gmail.com>
2026-02-10 17:37:21 +0100

    Rajout de commentaires pour types "irl" des variables (mètres, radians, etc...)

--- Projet_Clean/core/types.py
+++ Projet_Clean/core/types.py
@@ -1,18 +1,18 @@
-from dataclasses import dataclass
+from dataclasses import dataclass # on utilise dataclass juste pour simplifier le code de chaque classe, ca enlève de la redondance, pas besoind d'ecrire un constructeur a chaque fois
 
 @dataclass
 class Pos2D:
     """Position et orientation du robot dans la simulation"""
-    x: float
-    y: float
+    x: float # mètres
+    y: float # mètres
     orientation: float  # radians, orientation = 0 -> droite
 
 
 @dataclass
 class CommandeRoues:
     """Commandes envoyées aux roues"""
-    vitesse_rotation_gauche: float  # rad/s
-    vitesse_rotation_droite: float
+    vitesse_rotation_gauche: float # rad/s
+    vitesse_rotation_droite: float # 
 
 
 @dataclass
@@ -20,7 +20,7 @@ class EtatRoues:
     """
     Etat "réel" des roues
     """
-    vitesse_rotation_gauche: float
-    vitesse_rotation_droite: float
-    rotation_totale_gauche: float
-    rotation_totale_droite: float
+    vitesse_rotation_gauche: float # rad/s
+    vitesse_rotation_droite: float #
+    rotation_totale_gauche: float # rad
+    rotation_totale_droite: float #
