c79cf9d41fed040f2b3fd21c09ca2c551d47d192
x1angkun05 <xiangkunssr15@gmail.com>
2026-02-16 22:22:39 +0800

    Update monde.py

--- Projet_Clean/monde.py
+++ Projet_Clean/monde.py
@@ -1,5 +1,6 @@
 # On pourrait mettre ici tout ce qui concerne l'interface, les obstacles, etc
 import config as cfg
+import math
 
 class Obstacle:
     def __init__(self,x,y,largeur,longueur):
@@ -45,3 +46,35 @@ class Monde:
         
         return False  # Pas de collision
         
+   
+
+    def lire_distance_devant(self, pos_robot, portee_max=2.0):
+        """
+        Capteur radar ultrasonique analogique
+        """
+        x_m, y_m = pos_robot.x, pos_robot.y 
+        ori = pos_robot.orientation 
+        
+        # check pour chaque 0.05 metre
+        pas = 0.05 
+        for d in range(1, int(portee_max / pas)):
+            dist_test = d * pas
+            # Calculer les coordonnées (en mètres) du point de détection.
+            test_x_m = x_m + dist_test * math.cos(ori)
+            test_y_m = y_m + dist_test * math.sin(ori)
+            
+            # Convertir en coordonnées pixel pour correspondre à la position de l'obstacle
+            test_x_px = test_x_m * cfg.SCALE
+            test_y_px = test_y_m * cfg.SCALE
+            
+            for obs in self.obstacles:
+                # 
+                obs_L = obs.longueur * cfg.SCALE / 2
+                obs_l = obs.largeur * cfg.SCALE / 2
+                
+                # Détection de collision
+                if (abs(test_x_px - obs.x) < obs_L and 
+                    abs(test_y_px - obs.y) < obs_l):
+                    return dist_test
+                    
+        return portee_max # Si aucun obstacle n'est détecté, revenir à la pleine échelle.
