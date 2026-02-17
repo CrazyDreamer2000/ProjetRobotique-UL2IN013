daa944884c33d398168d50e8123084fe7f200dea
x1angkun05 <xiangkunssr15@gmail.com>
2026-02-18 03:57:59 +0800

    rename

---
+++ Projet_Clean_myself/core/robot.py
@@ -0,0 +1,92 @@
+import math
+from .types import Pos2D, CommandeRoues, EtatRoues
+from .cinematique import CinematiqueDeuxRoues
+
+class Robot:
+    """Le "core" du robot"""
+    def __init__(self, rayon_roue_m: float, ecartement_roues_m: float):
+        
+        # Modèle qui traduit les vitesses des roues en déplacement du robot
+        self.modele_mouvement = CinematiqueDeuxRoues(rayon_roue_m, ecartement_roues_m)
+
+        # Position & angle du robot (séparé du core)
+        self.pos = Pos2D(0.0, 0.0, 0.0)
+
+        # Commandes envoyées aux roues
+        self.commande = CommandeRoues(0.0, 0.0)
+
+        # Etat réel des roues (juste pour avoir code plus réaliste)
+        self.roues = EtatRoues(
+            vitesse_rotation_gauche=0.0,
+            vitesse_rotation_droite=0.0,
+            rotation_totale_gauche=0.0,
+            rotation_totale_droite=0.0,
+        )
+        self.accel_lineaire = 0.0
+        self.vitesse_precedante = 0.0 #pour calculer l'acceleration
+        self.dist_obstacle = 2.0  #la plus grand distance on peut detecter
+        
+        # Flag pour déterminer si le robot est en collision
+        self.en_collision = False
+
+    def definir_commande_roues(self, vitesse_rotation_gauche: float, vitesse_rotation_droite: float):
+        self.commande = CommandeRoues(vitesse_rotation_gauche, vitesse_rotation_droite)
+
+    def step(self, dt: float, monde=None):
+        """
+        Avance la simulation de dt secondes
+        - met à jour les roues
+        - calcule le mouvement
+        - met à jour la pos (avec vérification des collisions)
+        
+        Paramètres:
+            dt: Temps écoulé en secondes
+            monde: Instance de Monde pour vérifier les collisions (optionnel)
+        """
+
+        # Envoyer la commande (vitesse rotation) aux roues
+        self.roues.vitesse_rotation_gauche = self.commande.vitesse_rotation_gauche
+        self.roues.vitesse_rotation_droite = self.commande.vitesse_rotation_droite
+
+        # Met a jour la rotation totale des roues (on rajoute l'angle obtenu en dt temps)
+        self.roues.rotation_totale_gauche += self.roues.vitesse_rotation_gauche * dt
+        self.roues.rotation_totale_droite += self.roues.vitesse_rotation_droite * dt
+
+        # Calcul du mouvement du robot
+        vitesse_avant, vitesse_rotation = self.modele_mouvement.vitesses_robot_depuis_roues(
+            self.roues.vitesse_rotation_gauche,
+            self.roues.vitesse_rotation_droite
+        )
+
+        self.maj_capteurs(dt, monde, vitesse_avant)
+
+        # Calculer la nouvelle position
+        pos_suiv = self.modele_mouvement.avance_pos(self.pos, vitesse_avant, vitesse_rotation, dt)
+        
+        # Vérifier les collisions
+        if monde is not None and monde.collisions_robot(pos_suiv):
+            # Collision détectée: arrêter le robot
+            self.definir_commande_roues(0, 0)
+            self.en_collision = True
+            # La position ne change pas : on garde self.pos
+        else:
+            # Pas de collision: mettre à jour la position
+            self.pos = pos_suiv
+            self.en_collision = False
+
+        # Normaliser l'orientation dans [-pi, +pi] 
+        self.pos.orientation = (self.pos.orientation + math.pi) % (2 * math.pi) - math.pi
+
+
+    def maj_capteurs(self, dt, monde, vitesse_actuelle):
+        """renouvMettre à jour l'état du capteur du robot"""
+
+        "(a = Δv / Δt)"
+        if dt > 0:
+            self.accel_lineaire = (vitesse_actuelle - self.vitesse_precedante) / dt
+        self.vitesse_precedante = vitesse_actuelle
+
+        if monde is not None:
+            self.dist_obstacle = monde.lire_distance_devant(self.pos)
+        else:
+            self.dist_obstacle = 5.0 #il y a pas obstacle
