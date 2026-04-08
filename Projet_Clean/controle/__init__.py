feec4b21b80d43a20263bd4c83e08862be505128
tkevin02 <tratratkevin@gmail.com>
2026-04-08 10:35:50 +0200

    Add AlgoHexa to algorithm imports and registry

--- Projet_Clean/controle/__init__.py
+++ Projet_Clean/controle/__init__.py
@@ -4,11 +4,13 @@
 #from .AlgoEviter import AlgoEviter
 from .algo_carre import AlgoCarre
 from .algo_carre_safe import AlgoCarreSafe
+from .algo_hexa import AlgoHexa
 
 # Algorithmes
 ALGOS = {
             "carre" : AlgoCarre,
             "carresafe" : AlgoCarreSafe,
+            "hexa" : AlgoHexa,
             #"tourner" : AlgoTournerSurPlace,
             #"eviter" : AlgoEviter,
             #"arret" : AlgoArretDevantObstacle,
