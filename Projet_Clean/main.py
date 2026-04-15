0b0fea6c95dd1a3d3be7e983a54597f5d2e15f31
Alex <guapelididoo@gmail.com>
2026-04-15 15:14:59 +0200

    Clean up du main et adaptation pour qu'il marche avec TraducteurReel au besoin

--- Projet_Clean/main.py
+++ Projet_Clean/main.py
@@ -2,40 +2,54 @@ import time
 from parser import parse_args
 import config as cfg
 from controle import ALGOS
-from controle.traducteur import TraducteurSimu
-from core.robot import Robot
-from monde.monde import Obstacle, Monde
-from threading import RLock # RLock = verrou partage entre le thread principal et l'affichage.
-from affichage.Affichage import Affichage # Classe du thread qui gere la fenetre et le rendu.
+
+Simu = True
+
+robot = None
+monde = None
+trad = None
 
 parseArgs = parse_args() # Lit les arguments de la ligne de commande et renvoie un Namespace , un contenuer avec les valeurs lit
 
-robot = Robot(cfg.RAYON_ROUE, cfg.ECARTEMENT_ROUES, parseArgs.orientation, cfg.LONGUEUR_MONDE / 2, cfg.LARGEUR_MONDE / 2)
-monde = Monde(robot) 
-trad = TraducteurSimu(monde)
+if Simu:
+    from core.robot import Robot
+    from monde.monde import Monde
+    from threading import RLock # RLock = verrou partage entre le thread principal et l'affichage.
+    from affichage.Affichage import Affichage # Classe du thread qui gere la fenetre et le rendu.
+    from traducteur import TraducteurSimu
+
+    robot = Robot(cfg.RAYON_ROUE, cfg.ECARTEMENT_ROUES, parseArgs.orientation, cfg.LONGUEUR_MONDE / 2, cfg.LARGEUR_MONDE / 2)
+    monde = Monde(robot) 
+    trad = TraducteurSimu(monde)
+
+    lock = RLock() # Verrou partage entre simulation (main) et rendu (thread affichage).
+    vue = Affichage(robot, monde, lock) # On cree l'affichage en lui donnant robot/monde/lock.
+    vue.start() # Demarre le thread d'affichage en parallele du main.
+
+else:
+    from robot2I013 import robot2I013
+    from traducteur import TraducteurReel
+    robot = robot2I013()
+    trad = TraducteurReel(robot)
 
 algo = ALGOS[parseArgs.algo](trad, 10, 0.5) # Algo choisi par defaut dans code actuel.
 algo.start()
-
-lock = RLock() # Verrou partage entre simulation (main) et rendu (thread affichage).
-vue = Affichage(robot, monde, lock) # On cree l'affichage en lui donnant robot/monde/lock.
-vue.start() # Demarre le thread d'affichage en parallele du main.
 
 running = True 
 
 while running:    
     
-    if not vue.running:   # Si l'utilisateur ferme la fenetre dans le thread affichage, on stoppe ici aussi.
-        running = False
+    if Simu:
+        if not vue.running:   # Si l'utilisateur ferme la fenetre dans le thread affichage, on stoppe ici aussi.
+            running = False
 
-    # on protege l'acces aux donnees partagees.
-    with lock:
+        with lock:  # on protege l'acces aux donnees partagees.
+            algo.step()
+            monde.step()  # On avance la physique du robot de dt secondes.
+    else:
         algo.step()
-        monde.step()  # On avance la physique du robot de dt secondes.
-    
+
     time.sleep(1/60)
 
 vue.stop()  # Fin de boucle: on demande au thread d'affichage de s'arreter.
 vue.join(timeout=1.0) # Puis on attend sa fin pour une fermeture propre.
-
-pygame.quit()
