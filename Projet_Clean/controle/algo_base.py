fe929c033fcee62374d2dc6583595a43ee918eae
Rayane YOUSFI <rayaneoneplus@gmail.com>
2026-04-22 14:39:09 +0200

    Redefinition of the algobase class to help with debugging, and will make the code more efficient

--- Projet_Clean/controle/algo_base.py
+++ Projet_Clean/controle/algo_base.py
@@ -4,14 +4,19 @@ class AlgoBase:
     """
     Classe de base pour toutes les stratégies / primitives
     """
-    def __init__(self, traducteur):
+    def __init__(self, traducteur,name:str,type:str,modeDebug:bool=False):
         # Le traducteur est injecté à l'initialisation.
         self.trad = traducteur
+        self.name = name
+        self.type = type
+        self.modeDebug = modeDebug
 
     def start(self):
         """
         Initialise ou réinitialise la stratégie
         """
+        if self.modeDebug:
+            print(f"Initialisation de {self.name} ({self.type})")
         pass
 
     def step(self):
@@ -19,6 +24,9 @@ class AlgoBase:
         Exécute une étape de controle.
         Cette méthode envoie directement les commandes au robot.
         """
+        if self.modeDebug:
+            print(f"Step de {self.name} ({self.type})")
+
         if self.stop():
             self.trad.set_vitesse_roues(0.0, 0.0)
             return
@@ -27,4 +35,12 @@ class AlgoBase:
         """
         Retourne True si la stratégie doit s'arrêter / est terminée, False sinon (?)
         """
+        if self.modeDebug:
+            print(f"Check stop de {self.name} ({self.type})")
         pass
+
+    def __str__(self):
+        return f"{self.name} ({self.type})"
+    
+    def __repr__(self):
+        return self.__str__()
