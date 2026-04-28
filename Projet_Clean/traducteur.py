73398a4a399c7ae8cbb74fa4c8227a290a72f7f7
Rayane YOUSFI <rayaneoneplus@gmail.com>
2026-04-28 22:04:24 +0200

    Made functions clearer, and do what their name implies they do, for those which weren't, added functions to change,get,reset distance_sensor angle(), have to be implemented on TraducteurSimu though

--- Projet_Clean/traducteur.py
+++ Projet_Clean/traducteur.py
@@ -37,8 +37,17 @@ class Traducteur(ABC):
     @abstractmethod
     def est_en_collision(self) -> bool:
         pass
+    
+    @abstractmethod
+    def reset_angle_tete_capteur(self):
+        pass   
 
-    def tourner_capteur_robot(self,angle : float):
+    @abstractmethod
+    def tourner_tete_capteur(self, angle: float):
+        pass
+
+    @abstractmethod
+    def get_angle_tete_capteur(self) -> float:
         pass
 
 class TraducteurSimu(Traducteur):
@@ -53,8 +62,6 @@ class TraducteurSimu(Traducteur):
         self._distance_depart_gauche = 0.0
         self._distance_depart_droite = 0.0
         self._orientation_depart = monde.robot.pos.orientation
-
-        print("Traducteur créé")
 
     # COMMANDES
 
@@ -113,11 +120,24 @@ class TraducteurSimu(Traducteur):
         """
         return self.monde.robot.en_collision
     
-    def tourner_capteur_robot(self,angle : float):
+    def reset_angle_tete_capteur(self):
+        """ Réinitialise la tête du capteur à la position centrale (90°).
+        0 si tourné complètement à gauche, 180 si tourné complètement à droite.
         """
-        Tourne le capteur du robot de l'angle spécifié.
-        """
+        pass
 
+    def tourner_tete_capteur(self, angle: float):
+        """ Tourne la tête du capteur à l'angle spécifié.
+        Paramètre:
+            - angle : float, angle de 0 à 180 degrés (90 = milieu)
+            0 si tourné complètement à gauche, 180 si tourné complètement à droite.
+        """
+        pass
+
+    def get_angle_tete_capteur(self) -> float:
+        """ Retourne l'angle actuel de la tête du capteur (en degrés)."""
+        pass  
+    
 
 class TraducteurReel(Traducteur):
     """
@@ -126,106 +146,134 @@ class TraducteurReel(Traducteur):
     """
     def __init__(self, robot):
         self.robot = robot
-    # Pour la distance
-        self._ref_pos_gauche_distance = 0.0
-        self._ref_pos_droite_distance = 0.0
-    # Pour l'angle
-        self._ref_angle_gauche = 0.0
-        self._ref_angle_droite = 0.0
-    # Pour l'angle du capteur
-        self._angle_capteur = 0.0
+
+        # Références de départ pour les mesures relatives, 
+
+        self.dist_ref_gauche = 0.0
+        self.dist_ref_droite = 0.0  
+
+        self.angle_ref_gauche = 0.0
+        self.angle_ref_droite = 0.0
+        
+        # Compteurs explicites
+        self._distance_parcourue_depuis_reset = 0.0
+        self._angle_parcouru_depuis_reset = 0.0
+        
+        # Degré du capteur de distance, default = 90 (milieu), 0 = complètement à gauche, 180 = complètement à droite
+        self._angle_capteur = 90
+
+    # COMMANDES
 
     def set_vitesse_roues(self, v_gauche: float, v_droite: float):
         """
-        Ici on effectue la conversion de rad/s à deg/s
-        v_gauche: Vitesse de la roue gauche (en rad/s)
-        v_droite: Vitesse de la roue droite (en rad/s)
+        Paramètres:
+            - v_gauche : float
+              Vitesse de rotation de la roue gauche (rad/s)
+            - v_droite : float
+              Vitesse de rotation de la roue droite (rad/s)
         """
-        # formule de degré par radian
-        deg_par_rad = 180 / math.pi #57.296
+        # Conversion rad/s → deg/s
+        deg_par_rad = 180 / math.pi
+        dps_gauche = v_gauche * deg_par_rad
+        dps_droite = v_droite * deg_par_rad
 
-        #Ici on effectue la conversion rad/s à deg/s
-        dps_gauche=  v_gauche  *  deg_par_rad
-        dps_droite = v_droite *  deg_par_rad
+        # Application aux moteurs
+        self.robot.set_motor_dps(self.robot.MOTOR_LEFT, dps_gauche)
+        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT, dps_droite)
 
-        #ici on applique la vitesse en deg/s au moteurs gauche et droite
-        self.robot.set_motor_dps(self.robot.MOTOR_LEFT,dps_gauche)
-        self.robot.set_motor_dps(self.robot.MOTOR_RIGHT,dps_droite) 
-
-    def get_distance_devant(self) -> float:
-        """  
-        Sortie:
-            - (Fonction a nettoyer)
-            - distance_devant(robot. : float)
-              Valeur du capteur de distance (distance en milimètres : 2m, 2000mm max)
-        """
-        #On lit le capteur de distance du robot, qui renvoie distance entière en milimètre,
-        distance_devant= self.robot.get_distance()
-        #A voir dans doc de la classe, mais ici l'intervalle est de **5-8,000** millimeters.
-        if distance_devant == 8190:
-            return 2000.0  #On retourne 2000mm si la valeur est à 8190, ce qui correspond à une distance supérieure à 2m (limite du capteur)
-        return distance_devant
+    # DISTANCE PARCOURUE
 
     def reset_distance_parcourue(self):
         """Réinitialise les références de distance pour le calcul de la distance parcourue."""
-        #get_motor_position renvoie un couple (degré,degré)
-        encodeurs=self.robot.get_motor_position()
-        self._ref_pos_gauche_distance = encodeurs[0]
-        self._ref_pos_droite_distance = encodeurs[1]
+        encodeurs = self.robot.get_motor_position()
+        self.dist_ref_gauche = encodeurs[0]
+        self.dist_ref_droite = encodeurs[1]
+        self._distance_parcourue_depuis_reset = 0.0
 
     def get_distance_parcourue(self) -> float:
-        """Calcule la distance parcourue depuis le dernier reset."""
-        rayon = self.robot.WHEEL_DIAMETER / 2.0 # On recupere le rayon de la roue (diamètre/2)
+        """Calcule la distance parcourue depuis le dernier reset en utilisant les rotations des roues."""
+        rayon = self.robot.WHEEL_DIAMETER / 2.0  # mm
 
-        #get_motor_position renvoie un couple (degré,degré)
-        encodeurs=self.robot.get_motor_position()                   
-        delta_gauche = encodeurs[0] - self._ref_pos_gauche_distance
-        delta_droite = encodeurs[1] - self._ref_pos_droite_distance
-        
-        #distance = angle * rayon (ici on convertit dégrés en radian pour distance metres)
-        distance_gauche = delta_gauche * (math.pi / 180) * rayon
-        distance_droite = delta_droite * (math.pi / 180) * rayon
-        #on retourne la distance moyenne parcourue par les deux roues
-        distance_moy_mm= abs(distance_gauche + distance_droite) / 2
-        return distance_moy_mm 
+        encodeurs = self.robot.get_motor_position()
+        delta_gauche_deg = encodeurs[0] - self.dist_ref_gauche
+        delta_droite_deg = encodeurs[1] - self.dist_ref_droite
+
+        # Conversion degrés → radians
+        delta_gauche_rad = delta_gauche_deg * (math.pi / 180)
+        delta_droite_rad = delta_droite_deg * (math.pi / 180)
+
+        distance_gauche = delta_gauche_rad * rayon  # mm
+        distance_droite = delta_droite_rad * rayon  # mm
+        self._distance_parcourue_depuis_reset = abs(distance_gauche + distance_droite) / 2
+        return self._distance_parcourue_depuis_reset
+
+    # ANGLE PARCOURU
 
     def reset_angle_parcouru(self):
         """Réinitialise la référence d'orientation pour le calcul de l'angle parcouru."""
         encodeurs = self.robot.get_motor_position()
-        self._ref_angle_gauche = encodeurs[0]
-        self._ref_angle_droite = encodeurs[1]
+        self.angle_ref_gauche = encodeurs[0]
+        self.angle_ref_droite = encodeurs[1]
+        self._angle_parcouru_depuis_reset = 0.0
 
     def get_angle_parcouru(self) -> float:
-        """ Calcule l'angle parcouru depuis le dernier reset."""
+        """Calcule l'angle parcouru depuis le dernier reset en utilisant les encodeurs."""
         encodeurs = self.robot.get_motor_position()
-        
-        delta_gauche = encodeurs[0] - self._ref_angle_gauche
-        delta_droite = encodeurs[1] - self._ref_angle_droite
-        
-        # Conversion degrés → radians
-        delta_gauche_rad = delta_gauche * (math.pi / 180)
-        delta_droite_rad = delta_droite * (math.pi / 180)
-        
-        # Rayon de la roue (mm)
-        rayon = self.robot.WHEEL_DIAMETER / 2.0
-        
-        # Distance parcourue par chaque roue (mm)
-        distance_gauche = delta_gauche_rad * rayon
-        distance_droite = delta_droite_rad * rayon
-        #on retourne l'angle parcouru
-        dist_roues = self.robot.WHEEL_BASE_WIDTH  # distance de roue gauche a droite
-        angle_rad = (distance_droite - distance_gauche) / dist_roues
-    
-        return angle_rad
+        delta_gauche_deg = encodeurs[0] - self.angle_ref_gauche
+        delta_droite_deg = encodeurs[1] - self.angle_ref_droite
+
+        rayon = self.robot.WHEEL_DIAMETER / 2.0  # mm
+        dist_entre_roues = self.robot.WHEEL_BASE_WIDTH  # mm
+
+        # Formule de l'angle parcouru (en radians)
+        angle_rad = (delta_droite_deg - delta_gauche_deg) * (rayon / dist_entre_roues) * (math.pi / 180)
+        self._angle_parcouru_depuis_reset = angle_rad
+        return self._angle_parcouru_depuis_reset
+
+    # CAPTEURS / ETAT
+
+    def get_distance_devant(self) -> float:
+        """
+        Sortie:
+            - float : distance en millimètres (Intervalle du capteur: 5 → 8000 mm)
+        """
+        distance = self.robot.get_distance()
+        if distance == 8190:  # Valeur spéciale indiquant que le capteur n'a rien détecté (au-delà de sa portée maximale, 5 - 8000mm)
+            return 8000.0  # On considère qu'il n'y a rien
+        return float(distance)
 
     def est_en_collision(self) -> bool:
-        """ Indique si le robot est en collision en se basant sur la distance devant et un seuil de collision."""
-        SEUIL_COLLISION = 200.0  # Seuil de distance en millimètres pour considérer une collision
-        if self.get_distance_devant() < SEUIL_COLLISION: 
-            return True
-        return False
+        """
+        Renvoie True si le robot est en collision, False sinon.
+        Seuil à 200 mm (20 cm).
+        """
+        SEUIL_COLLISION = 200.0  # mm
+        return self.get_distance_devant() < SEUIL_COLLISION
 
-    def tourner_capteur_robot(self,angle : float):
-        """ Tourne le capteur du robot de l'angle spécifié """
-        self._angle_capteur = angle
+
+    def reset_angle_tete_capteur(self):
+        """Réinitialise la tête du capteur à la position centrale (90°).
+        0 si tourné complètement à gauche, 180 si tourné complètement à droite.
+        """
+        self._angle_capteur = 90
         self.robot.servo_rotate(self._angle_capteur)
+
+    def tourner_tete_capteur(self, angle: float):
+        """
+        Tourne la tête du capteur à l'angle spécifié.
+        Paramètre:
+            - angle : float, angle de 0 à 180 degrés (90 = milieu)
+            0 si tourné complètement à gauche, 180 si tourné complètement à droite.
+        """
+        if angle >= 0 and angle <= 180:
+            self._angle_capteur = angle
+            self.robot.servo_rotate(self._angle_capteur)
+        else:
+            print("Angle de tête de capteur invalide : doit être entre 0 et 180 degrés.")
+
+    def get_angle_tete_capteur(self) -> float:
+        """
+        Retourne l'angle actuel de la tête du capteur (en degrés).
+        """
+        return self._angle_capteur
+    
